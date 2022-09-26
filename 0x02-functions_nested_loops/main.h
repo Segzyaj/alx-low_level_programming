28c1e2dc9c6e0d372a8bde4c6f54466517cf1e25
Segzyaj <ajetunmobioluwasegun@gmail.com>
2022-09-25 19:34:42 -0700

    Task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-i findef MAIN.H                                                                                          
+ifindef MAIN.H
 #define MAIN.H                                                                                             
                                                                                                         
 void print_alphabet(void);                                                                                 
@@ -13,8 +13,7 @@ void jack_bauer(void);
 void times_table(void);                                                                                
 int add(int, int);                                                                                         
 void print_to_98(int n);                                                                                   
-void print_times_table(int n);                                                                             
-                                                                                                          
+void print_times_table(int n);
 
 #endif                                                                                                     
 
